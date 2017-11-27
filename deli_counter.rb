0386ee75b7cbafd70248c330ff108d8f04cfb971
bennybop <bennybop74@yahoo.com>
2017-11-27 23:12:50 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,4 +1,17 @@
-def line(deli)
- puts "The line is currently empty."
- end
- 
+katz_deli = []
+number = 0
+
+def line(katz_deli)
+  if katz_deli.length == 0
+    puts "The line is currently empty"
+  else
+    line = "The line is currently:"
+    counter = 0
+    until counter == katz_deli.length
+      line << "#{katz_deli[counter]}"
+      counter+=1
+    end
+    puts line
+  end
+end
+
